ee208e52a189749999827bfb9252db0c80431afc
thth <ththfh@gmail.com>
2020-12-23 00:14:22 -0800

    I love how the problem is _how_ to brute force

--- 23.exs
+++ 23.exs
@@ -1,4 +1,5 @@
 defmodule TwentyThree do
+  # using Stream.cycle and lists
   def part_one(input) do
     input
     |> parse()
@@ -10,15 +11,21 @@ defmodule TwentyThree do
     |> Integer.undigits()
   end
 
+  # using a map ("cupmap") with keys: cups, values: next clockwise cup
+  # runs in ~45s
   def part_two(input) do
-    list = parse_two(input)
-    list
-    |> Stream.iterate(&move_two/1)
-    |> Enum.at(10)
-    |> Stream.cycle()
-    |> Stream.drop_while(&(&1 != 1))
-    |> Enum.slice(1, 2)
-    |> Enum.reduce(&*/2)
+    initial_current_cup = input |> parse() |> List.first()
+    initial_cupmap = setup_cupmap(input, 10..1_000_000)
+
+    {_final_current_cup, final_cupmap} =
+      {initial_current_cup, initial_cupmap}
+      |> Stream.iterate(&cupmap_move(&1, 1_000_000))
+      |> Enum.at(10_000_000)
+
+    first_star = final_cupmap[1]
+    second_star = final_cupmap[first_star]
+
+    first_star * second_star
   end
 
   defp parse(text) do
@@ -27,15 +34,27 @@ defmodule TwentyThree do
     |> Integer.digits()
   end
 
-  defp parse_two(text) do
-    original = parse(text)
-    start = Enum.max(original) + 1
-    original ++ Enum.to_list(start..1_000_000)
+  defp setup_cupmap(text, rest_start..rest_end) do
+    original_list = parse(text)
+
+    cupmap_initial =
+      original_list
+      |> List.insert_at(-1, rest_start)
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(&List.to_tuple/1)
+
+    cupmap_middle =
+      rest_start..(rest_end - 1)
+      |> Stream.map(fn n -> {n, n + 1} end)
+
+    cupmap_last = [{rest_end, List.first(original_list)}]
+
+    Stream.concat([cupmap_initial, cupmap_middle, cupmap_last])
+    |> Enum.into(%{})
   end
 
   defp move([current, a, b, c | rest] = list) do
-    sorted_rest = Enum.sort(rest, :desc)
-    destination = Enum.find(sorted_rest, &(&1 < current)) || Enum.at(sorted_rest, 0)
+    destination = find_destination(current, a, b, c, Enum.max(list))
     new_rest =
       [current | rest]
       |> Stream.cycle()
@@ -47,20 +66,24 @@ defmodule TwentyThree do
     |> Enum.slice(1, length(list))
   end
 
-  defp move_two([current, a, b, c | rest]) do
-    destination = find_next(current, a, b, c, 1_000_000)
-    new_rest =
-      [current | rest]
-      |> Stream.cycle()
-      |> Stream.drop_while(&(&1 != destination))
-      |> Enum.slice(1, 999_997)
-    [a, b, c | new_rest]
-    |> Stream.cycle()
-    |> Stream.drop_while(&(&1 != current))
-    |> Enum.slice(1, 1_000_000)
+  defp cupmap_move({curr, cupmap}, total_cups) do
+    move1 = cupmap[curr]
+    move2 = cupmap[move1]
+    move3 = cupmap[move2]
+    next_curr = cupmap[move3]
+    destination = find_destination(curr, move1, move2, move3, total_cups)
+    after_destination = cupmap[destination]
+
+    new_cupmap =
+      cupmap
+      |> Map.put(curr, next_curr)
+      |> Map.put(destination, move1)
+      |> Map.put(move3, after_destination)
+
+    {next_curr, new_cupmap}
   end
 
-  defp find_next(current, a, b, c, max) do
+  defp find_destination(current, a, b, c, max) do
     cond do
       (n = cycle(current - 1, max)) not in [a, b, c] -> n
       (n = cycle(current - 2, max)) not in [a, b, c] -> n
@@ -69,22 +92,16 @@ defmodule TwentyThree do
     end
   end
 
-  defp cycle(n, max) do
-    if n > 0 do
-      n
-    else
-      max - abs(n)
-    end
-  end
+  defp cycle(n, _) when n > 0, do: n
+  defp cycle(n, max), do: max - abs(n)
 end
 
 input = File.read!("input/23.txt")
-# # input = File.read!("input/help.txt")
 
 input
 |> TwentyThree.part_one()
 |> IO.inspect()
 
-# input
-# |> TwentyThree.part_two()
-# |> IO.inspect()
+input
+|> TwentyThree.part_two()
+|> IO.inspect()
